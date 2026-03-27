d2534cc52afc8dea7822e0b43e3ecbd638e72692
realdrivemultiplayer-droid <realdrivemultiplayer@gmail.com>
2026-03-27 13:24:33 -0400

    Update logs.js

--- dynamo-bot/modules/logs.js
+++ dynamo-bot/modules/logs.js
@@ -51,7 +51,7 @@ async function send(guild, embed) {
  */
 function base(title, guild) {
   return new EmbedBuilder()
-    .setColor('#ED4245') // Rojo estético
+    .setColor('#c816f0') // Rojo estético
     .setTitle(`${title}`)
     .setFooter({ 
       text: `Logs • ${guild.name}`, 
